ee64b16fb1da42009316019cb02ddbd503ccbcdf
Alexander Kuvaev <alexander@kuvaev.me>
2017-06-01 00:57:12 +0300

    Fix FPS counter

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import pygame, sys, visualizer, game, camera, time
+import pygame, sys, visualizer, game, camera
 from pygame.locals import *
 
 def main():
@@ -8,9 +8,6 @@ def main():
     HEIGHT = 768
 
     my_font = pygame.font.SysFont("Courier", 16)
-    frame_count = 0
-    frame_rate = 0
-    t0 = time.clock()
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Tower Control")
@@ -23,6 +20,7 @@ def main():
     main_cam.reset(my_game.width, my_game.height, 
         my_game.tilewidth, my_game.tileheight)
 
+    clock = pygame.time.Clock()
     while True:
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
@@ -31,22 +29,17 @@ def main():
             if event.key == K_q:
                 pygame.event.post(pygame.event.Event(QUIT))
 
-        frame_count += 1
-        if frame_count % 10 == 0:
-            t1 = time.clock()
-            frame_rate = 10 / (t1-t0)
-            t0 = t1
-
         my_game.update()
         main_cam.update()
         vis.draw(screen, my_game, main_cam)
 
-        the_text = my_font.render("FPS = {0:.2f} fps"
-                  .format(frame_rate), True, (100, 0, 0))
+        
+        the_text = my_font.render("FPS = {0:.2f}"
+                  .format(clock.get_fps()), True, (100, 0, 0))
         screen.blit(the_text, (10, 10))
 
         pygame.display.flip()
-        # pygame.time.wait(1)
+        clock.tick(60)
 
     pygame.quit()
 
